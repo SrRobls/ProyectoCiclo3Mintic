e81d9f3f4a542887721bae4b058af27eb8076eff
Johan Robles <johanrobles66@yahoo.com>
2022-09-17 14:30:04 -0500

    Iniciando framework Spring Boot

---
+++ los-campesinos/src/test/java/com/loscampesinos/loscampesinos/LosCampesinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loscampesinos.loscampesinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosCampesinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
