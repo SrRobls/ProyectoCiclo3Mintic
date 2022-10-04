90348da7b18d5e5964b6ce3e29c5ac21da61d74e
Johan Robles <johanrobles66@yahoo.com>
2022-10-04 08:10:22 -0500

    nueva base de datos

--- los-campesinos/src/main/java/com/loscampesinos/loscampesinos/controller/dto/RequestLogin.java
+++ los-campesinos/src/main/java/com/loscampesinos/loscampesinos/controller/dto/RequestLogin.java
@@ -1,7 +1,5 @@
 package com.loscampesinos.loscampesinos.controller.dto;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
